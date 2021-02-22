445e27e0adfa034647703bfc7f8a88ceee8986e2
Marco Seabra <marcoaugusto.sonda@br.com.br>
2021-02-22 11:58:52 -0300

    Setup inicial

---
+++ src/native-test/java/java/com/example/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package java.com.example;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
